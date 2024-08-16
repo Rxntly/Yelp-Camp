250ce5d23f682458a043d3ac0e958b83d3b750a7
ryan <rantlytufail@gmail.com>
2024-08-16 16:19:22 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# YelpCamp
+ My attempt at the Yelp Camp Project for the Web Developer Bootcamp 2024
---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground'); // Ensure this path is correct
+const ejsMate = require('ejs-mate');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("database connected");
+});
+
+const app = express();
+
+app.engine('ejs', ejsMate);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({extended: true}));
+
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.get('/campgrounds', async (req, res) => {
+    try {
+        const campgrounds = await Campground.find({});
+        res.render('campgrounds/index', { campgrounds }); 
+    } catch (err) {
+        console.error(err);
+        res.status(500).send("Internal Server Error");
+    }
+});
+
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+});
+
+app.post('/campgrounds' , async (req,res)=>{
+    const campground= new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
+})
+
+
+app.get('/campgrounds/:id', async(req,res,) => {
+    const campground=await Campground.findById(req.params.id)
+    res.render('campgrounds/show', {campground});
+})
+
+app.listen(3030, () => { 
+    console.log('serving on port 3030');
+});
