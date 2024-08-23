6376d615c08cff86c1290a9219cfb575982c6c95
ryan <rantlytufail@gmail.com>
2024-08-23 14:52:26 +0300

    Updated much of the CSS, fixed the structure of the files & the review function now works as intended.

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campground'); 
 const methodOverride = require('method-override');
 const ejsMate = require('ejs-mate');
 const ExpressError = require('./utils/ExpressError');
-const review =require("./models/review");
+const campgrounds = require('./routes/campgrounds');
 const catchAsync = require('./utils/catchAsync');
+const Campground = require('./models/campground');
+const Review = require('./models/review');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp');
 
@@ -22,95 +23,34 @@ app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
+
+app.use('/campgrounds', campgrounds);
 
 app.get('/', (req, res) => {
     res.render('home');
 });
 
-app.get('/campgrounds', async (req, res) => {
-    try {
-        const campgrounds = await Campground.find({});
-        res.render('campgrounds/index', { campgrounds }); 
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Internal Server Error");
-    }
-});
-
-
-app.get('/campgrounds/new', (req, res) => {
-    res.render('campgrounds/new');
-});
-
-app.post('/campgrounds' , async (req,res,next)=>{
-    const campground= new Campground(req.body.campground);
-    await campground.save();
-    res.redirect(`/campgrounds/${campground._id}`)
-})
-
-
-app.get('/campgrounds/:id',catchAsync( async(req,res,) => {
-    const campground=await Campground.findById(req.params.id).populate('reviews');
-    console.log('campgrounds',campground)
-    res.render('campgrounds/show', {campground});
-}))
-
-app.get('/campgrounds/:id/edit', async(req,res)=>{
-    const campground=await Campground.findById(req.params.id)
-    res.render('campgrounds/edit', {campground});
-})
-
-app.put('/campgrounds/:id', async (req, res) => {
-    const { id } = req.params;
-    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
-    res.redirect(`/campgrounds/${campground._id}`);
-});
-
-app.put('/campgrounds/:id', async(req,res,) => {
-const{id}=req.params;
-const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})
-res.redirect(`/campgrounds/${campground._id}`)
-})
-
-app.delete('/campgrounds/:id', async(req,res)=>{
-    const {id}= req.params;
-    await Campground.findByIdAndDelete(id);
-    res.redirect("/campgrounds");
-})
-
-app.all('*', (req,res,next)=>{
-    next(new ExpressError('Page not found', 404))
-})
-
-app.use((req, res, next) => {
-    const err = new ExpressError('Page Not Found', 404);
-    next(err);
-});
-
 app.post('/campgrounds/:id/reviews', catchAsync(async (req, res) => {
-    console.log('Received review submission:', req.body.review);
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
     campground.reviews.push(review);
-    await review.save(); 
+    await review.save();
     await campground.save();
-    console.log('Review saved successfully');
     res.redirect(`/campgrounds/${campground._id}`);
 }));
 
-app.delete('/campgrounds/:id/reviews:reviewId', catchAsync(async(req,res)=>{
-    const {id, reviewId}=req.params;
-    await Campground.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
+app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => {
+    const { id, reviewId } = req.params;
+    await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
-    res.redirect(`/campgrounds/${id}`)
-}))
+    res.redirect(`/campgrounds/${id}`);
+}));
 
-
-app.all("*", (req, res, next) => {
-  next(new ExpressError(404, "Page not found"));
-}); 
+app.all('*', (req, res, next) => {
+    next(new ExpressError('Page Not Found', 404));
+});
 
 app.use((err, req, res, next) => {
     const { statusCode = 500 } = err;
@@ -118,6 +58,6 @@ app.use((err, req, res, next) => {
     res.status(statusCode).render('error', { err });
 });
 
-app.listen(3030, () => { 
+app.listen(3030, () => {
     console.log('serving on port 3030');
 });
